98b9f640a9661d6af6aaef25ecff7a4442a20719
a-gehlot <andrew.gehlot@gmail.com>
2022-04-26 14:49:59 -0400

    ajax functions for xml requests

--- src/index.js
+++ src/index.js
@@ -17,6 +17,48 @@ window.$l = function(argument) {
 
 }
 
+$l.extend = function() {
+    let merged = Object.assign({}, ...arguments);
+    return merged;
+}
+
+$l.ajax = (options) => {
+    const request = new XMLHttpRequest();
+    const defaults = {
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        method: "GET",
+        url: "",
+        success: () => { },
+        error: () => { },
+        data: {},
+    };
+    options = $l.extend(defaults, options);
+
+    request.open(options.method, options.url);
+    console.log(request);
+    request.onload = () => {
+        if (request.status === 200) {
+            options.success(request.response);
+        } else {
+            options.error(request.response);
+        }
+    }
+    console.log(request);
+    request.send(JSON.stringify(options.data));
+
+}
+
+
+toQueryString = (obj) => {
+    let result = "";
+    for (const prop in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+            result += `${prop}=${obj[prop]}&`;
+        }
+    }
+    return result.substring(0, result.length - 1);
+};
+
 registerDocReadyCallback = (func) => {
     if (!_docReady) {
         _docReadyCallbacks.push(func);
